e9b8476f5add1daf357b3732f80c5b93f85777d7
Dilan Conaher Suy Miranda <82337586+DCSM2407@users.noreply.github.com>
2025-04-03 08:29:57 -0600

    feat: operadores completos
    feat: operadores completos

---
+++ QuintoA_Analisis/OperadoresComparacion.py
@@ -0,0 +1,12 @@
+#Declaracion de Variables
+edad = 15
+nombre = "williams"
+
+print("\nOperadores De Compracion")
+print("aplicando igualdad")
+print(edad == 25)
+print(edad == 15)
+
+print("\naplicando diferente")
+print(edad != 25)
+print(edad != 15)
