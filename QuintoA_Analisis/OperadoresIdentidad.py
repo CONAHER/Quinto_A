7ac83395e486441a6bf89a5dc1e013299772205b
Dilan Conaher Suy Miranda <82337586+DCSM2407@users.noreply.github.com>
2025-04-03 08:16:54 -0600

    todos los operadores
    todos los operadores

---
+++ QuintoA_Analisis/OperadoresIdentidad.py
@@ -0,0 +1,8 @@
+a = 5
+b = 6
+c = b
+print("Verificando A y B")
+print(a is b)
+print(c is b)
+print(a is not b)
+print(c is not b)
---
+++ QuintoA_Analisis/OperadoresPertenencia.py
@@ -0,0 +1,10 @@
+texto = "tecnologia"
+
+print("Operadores de Pertenencia")
+print("utilizando el in")
+print("t" in texto)
+print("z" in texto)
+
+print("\nutilinzando en not in")
+print("t" not in texto)
+print("z" not in texto)
